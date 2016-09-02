1bf8ece922cb8c38d0336d3a664607fbfdb09b3c
dgallichan <dgallichan@yahoo.com>
2016-09-02 16:59:06 +0200

    Update run_SiemensMP2RAGErecon.m
    minor modifications

--- run_SiemensMP2RAGErecon.m
+++ run_SiemensMP2RAGErecon.m
@@ -1,14 +1,13 @@
-% rawDataFile = '/home/gallicha/temp/autoTransferTemp/tempRaw/meas_MID76_mp2rage_1mm_FN1000b.dat';
 rawDataFile = '/home/gallicha/temp/autoTransferTemp/tempRaw/meas_MID36_mp2rage_FN600b_FatNav_06mm.dat';
 addpath(genpath('~/retroMoCoBox/'))
 addpath('~/matlabdownloads/spm12')
 run('~/matlabdownloads/fessler/setup.m')
 
-%% Fastest option: if you have loads of RAM (tested with 12 CPUs and 96 Gb of RAM on 1 mm data)
+%% Fastest option: if you have loads of RAM (tested with 12 CPUs and 96 Gb of RAM on both 1 mm and 600 um data)
 
 reconstructSiemensMP2RAGEwithFatNavs(rawDataFile,'bGRAPPAinRAM',1,'bKeepReconInRAM',1,'bFullParforRecon',1);
 
-%% if you have plenty of RAM (32 Gb works!):
+%% if you have plenty of RAM:
 
 % reconstructSiemensMP2RAGEwithFatNavs(rawDataFile,'bGRAPPAinRAM',1);
 
