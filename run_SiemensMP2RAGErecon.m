bbb65ddad4ac0b34707ab0048eb4211eae0d1578
dgallichan <dgallichan@yahoo.com>
2016-09-02 17:03:40 +0200

    Update run_SiemensMP2RAGErecon.m
    Improved comments

--- run_SiemensMP2RAGErecon.m
+++ run_SiemensMP2RAGErecon.m
@@ -1,6 +1,13 @@
+% Obviously this needs to point to your data (Siemens VB17 MP2RAGE with 3D-FatNavs)
 rawDataFile = '/home/gallicha/temp/autoTransferTemp/tempRaw/meas_MID36_mp2rage_FN600b_FatNav_06mm.dat';
+
+% And wherever you installed the Retro-MoCo-Box
 addpath(genpath('~/retroMoCoBox/'))
+
+% And SPM 12
 addpath('~/matlabdownloads/spm12')
+
+% And Prof. J. Fessler's Matlab toolbox (http://web.eecs.umich.edu/~fessler/code/) for the NUFFT
 run('~/matlabdownloads/fessler/setup.m')
 
 %% Fastest option: if you have loads of RAM (tested with 12 CPUs and 96 Gb of RAM on both 1 mm and 600 um data)
