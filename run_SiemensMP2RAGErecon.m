c103204912ef07a0aac431f1a4515a51bfb812fe
Daniel Gallichan <dgallichan@yahoo.com>
2017-08-09 14:58:51 +0100

    Update run_SiemensMP2RAGErecon.m

--- run_SiemensMP2RAGErecon.m
+++ run_SiemensMP2RAGErecon.m
@@ -1,4 +1,4 @@
-% Obviously this needs to point to your data (Siemens VB17 MP2RAGE with 3D-FatNavs)
+% Obviously this needs to point to your data (Siemens MP(2)RAGE with 3D-FatNavs)
 rawDataFile = '/home/gallicha/temp/autoTransferTemp/tempRaw/meas_MID36_mp2rage_FN600b_FatNav_06mm.dat';
 
 % And wherever you installed the Retro-MoCo-Box
@@ -8,9 +8,9 @@ addpath(genpath('~/retroMoCoBox/'))
 addpath('~/matlabdownloads/spm12')
 
 % And the Michigan Image Reconstruction Toolbox (MIRT) (http://web.eecs.umich.edu/~fessler/code/) for the NUFFT
-run('~/matlabdownloads/fessler/setup.m')
+run('~/matlabdownloads/mirt/setup.m')
 
-% Set the resolution of the FatNavs that were used
+% Set the resolution of the FatNavs that were used (default is 2 for 7T and 4 for 3T)
 FatNavRes_mm = 2;
 
 %% Fastest option: if you have loads of RAM (tested with 12 CPUs and 96 Gb of RAM on both 1 mm and 600 um data)
