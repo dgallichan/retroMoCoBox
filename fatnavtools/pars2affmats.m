2ac7c063419128770306f29b1c1749fda022f6ad
scedg10 <scedg10@wl020.cubric.cf.ac.uk>
2019-10-31 16:44:24 +0000

    use degrees instead of radians

--- fatnavtools/pars2affmats.m
+++ fatnavtools/pars2affmats.m
@@ -1,10 +1,12 @@
 function affmats = pars2affmats(pars)
 % function affmats = pars2affmats(pars)
 % 
-% pars is 6 x n matrix of mocopars
+% pars is 6 x n matrix of mocopars (3 translations, 3 rotations in degrees)
 % output is 4 x 4 x n affine matrices
 
 npars = size(pars,2);
+
+pars(4:6,:) = pars(4:6,:) * pi/180; % make into radians for SPM
 
 affmats = zeros(4,4,npars);
 
