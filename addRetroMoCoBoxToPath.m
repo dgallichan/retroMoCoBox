6c8c50231c947164f13bf8f36571df6e1ee569f0
scedg10 <gallichand@cardiff.ac.uk>
2021-12-15 10:57:32 +0000

    Update path to include mirt_nufft

--- addRetroMoCoBoxToPath.m
+++ addRetroMoCoBoxToPath.m
@@ -10,6 +10,9 @@ addpath([retroMoCoPath '/fatnavtools']);
 addpath([retroMoCoPath '/generaltools']);
 addpath([retroMoCoPath '/images']);
 addpath([retroMoCoPath '/niftitools']);
+addpath([retroMoCoPath '/mirt_nufft']);
+addpath([retroMoCoPath '/mirt_nufft/mex']);
+addpath([retroMoCoPath '/mirt_nufft/utilities']);
 if exist([retroMoCoPath '/mapVBVD_20160905'],'dir')
     addpath([retroMoCoPath '/mapVBVD_20160905']);
 end
