118836db10cdb65d8165d15967edfb589f5b0c75
Daniel Gallichan <gallichand@cardiff.ac.uk>
2022-06-17 12:15:00 +0100

    updates from pc

--- fatnavtools/apply_2DsimulatedMotion.m
+++ fatnavtools/apply_2DsimulatedMotion.m
@@ -14,15 +14,16 @@ useTable = 0;
 dataDims_mm = [1 1 1];
 fullMatrixDims = [Nres Nres 1];
 kspaceCentre = [Nres/2+1 Nres/2+1 1];
+oversampFactor = 2; % keep this high (i.e. around 2) for forward simulation of artifacts
 
 alignMats_swapTrans = alignMats;
 alignMats_swapTrans(1:3,4,:) = -alignMats_swapTrans(1:3,4,:);
 
 
 % run once just to be able to extract the 'st' object
-[~, st] = applyRetroMC_nufft_2D(kdata_in, alignMats_swapTrans, alignDim, alignIndices, useTable, dataDims_mm, fullMatrixDims, kspaceCentre);
+[~, st] = applyRetroMC_nufft_2D(kdata_in, alignMats_swapTrans, alignDim, alignIndices, useTable, dataDims_mm, fullMatrixDims, kspaceCentre,-1,oversampFactor);
 
 % and use the nufft rather than the nufft_adj function to simulate the rotations:
 kdata_simRotOnly = reshape(nufft(ifft2s(kdata_in),st),size(kdata_in));        
 % then apply just the translations:
-[~,~,kdata_simMotion] = applyRetroMC_nufft_2D(kdata_simRotOnly,alignMats_swapTrans,alignDim,alignIndices,useTable,dataDims_mm,fullMatrixDims,kspaceCentre);
+[~,~,kdata_simMotion] = applyRetroMC_nufft_2D(kdata_simRotOnly,alignMats_swapTrans,alignDim,alignIndices,useTable,dataDims_mm,fullMatrixDims,kspaceCentre,-1,oversampFactor);
