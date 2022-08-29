de2715f8f474b23ecbaac6d91abc64693f08165f
Daniel Gallichan <gallichand@cardiff.ac.uk>
2022-08-29 20:03:36 +0100

    add example of 2D simulated corruption and correction

--- fatnavtools/apply_2DsimulatedMotion.m
+++ fatnavtools/apply_2DsimulatedMotion.m
@@ -1,24 +1,26 @@
 function [kdata_simMotion, st] = apply_2DsimulatedMotion(kdata_in,mpars)
 
-Nres = size(mpars,2);
-alignMats = zeros(4,4,Nres);
+[Nx, Ny] = size(kdata_in);
+Npars = size(mpars,2);
+alignMats = zeros(4,4,Npars);
 this_mpars = mpars;
-for iT = 1:Nres
+for iT = 1:Npars
     alignMats(1:3,1:3,iT) = euler2rmat(0,0,this_mpars(3,iT));
     alignMats(1:2,4,iT) = this_mpars(1:2,iT);
 end
 alignMats(4,4,:) = 1;
 alignDim = 2;
-alignIndices = 1:Nres;
+alignIndices = 1:Npars;
 useTable = 0;
 dataDims_mm = [1 1 1];
-fullMatrixDims = [Nres Nres 1];
-kspaceCentre = [Nres/2+1 Nres/2+1 1];
+fullMatrixDims = [Nx Ny 1];
+kspaceCentre = [Nx/2+1 Ny/2+1 1];
 oversampFactor = 2; % keep this high (i.e. around 2) for forward simulation of artifacts
 
 alignMats_swapTrans = alignMats;
-alignMats_swapTrans(1:3,4,:) = -alignMats_swapTrans(1:3,4,:);
-
+alignMats_swapTrans(1:3,4,:) = -alignMats_swapTrans(1:3,4,:); % forward simulation applies translations in opposite direction to the correction
+                                                              % (rotations are swapped via nufft vs nufft adjoint)
+                                                              
 
 % run once just to be able to extract the 'st' object
 [~, st] = applyRetroMC_nufft_2D(kdata_in, alignMats_swapTrans, alignDim, alignIndices, useTable, dataDims_mm, fullMatrixDims, kspaceCentre,-1,oversampFactor);
